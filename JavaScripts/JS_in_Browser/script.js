655fc4fed67b0c1b83acc92e11f8f7008d8684f3
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-07 01:23:38 +0530

    Added how JS works on Browser

---
+++ JavaScripts/JS_in_Browser/script.js
@@ -0,0 +1,78 @@
+// console.log("Method 1");
+// // alert("Pirated Site")
+
+// // types of console in javascript
+
+// console.log(console);
+
+// console.log("Heyy Everyone");
+
+// console.error("This gives an error message");
+
+// console.assert(56 > 123);
+
+// // console.clear();
+
+// let obj = { a: 3, b: 1, c: 5, d: 4, e: 2 };
+// console.table(obj);
+
+// console.warn("Come back to Allah");
+
+// console.info("This is an importent infomation listen carefully");
+
+// console.time("a");
+// console.timeEnd("a");
+
+// console.log("printing using for loops");
+// console.time("time taken in for loops");
+// for (let i = 0; i < 5; i++) {
+//   console.log(34);
+// }
+// console.timeEnd("time taken in for loops");
+
+// let i = 0;
+// console.log("printing using while loops");
+// console.time("time taken in while loops");
+// while (i < 5) {
+//   console.log(34);
+//   i++;
+// }
+// console.timeEnd("time taken in while loops");
+
+// // Alert , Prompt , Confirm in JavaScript
+
+// // Alert
+// alert("Please enter your name ");
+
+// // prompt
+// let n = prompt("Enter your name");
+
+// n = Number.parseInt(n); // change string into integer
+
+// alert("The data type of the value your entered is : " + typeof n);
+// console.log(n);
+
+// // Confirm
+// let con = confirm("Are you sure you want to allow this page");
+// if (con) {
+//   document.write(n);
+// } else {
+//   document.write("Please allow this page");
+// }
+
+
+// BOM , DOM and Window Obejcts
+
+// DOM :- Document Object model
+window.console.log(window)
+
+// window.alert("this is alert alert")
+
+console.log(document.body)
+document.body.style.color="#FFFFFF"
+
+// BOM :- Browser Object model
+
+
+
+
