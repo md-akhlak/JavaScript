4c1409416faea1d95397ff70f8359e0688e41456
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-11 19:35:30 +0530

    today i learned funtions

--- JavaScripts/Basics_of_JavaScript/Functions.js
+++ JavaScripts/Basics_of_JavaScript/Functions.js
@@ -1,3 +1,13 @@
+/*
+Function Defination
+Calling a Function
+Function Parameter
+Funtion Argument
+Function Expression
+return keyword 
+Anonymous Function
+*/
+
 // Functions Defination
 /*
 A function definition (also called a function declaration or function statement) 
@@ -6,34 +16,66 @@ SYNTAX :- function functionName(parameters){
 }
 */
 
-// non-parameterised functions
-function sumTwo() {
-  var a = 10,
-    b = 30;
-  var total = a + b;
-  console.log(total);
-}
+// // non-parameterised functions
+// function sumTwo() {
+//   var a = 10,
+//     b = 30;
+//   var total = a + b;
+//   console.log(total);
+// }
 
-// Parameterised functions
-function sumOfTwo(x, y) {
-  // var a = 10 , b = 30;
-  let total = y + x;
-  console.log(total);
-}
-// console.log(sumOfTwo());  /// gives undefined
+// // Parameterised functions
+// function sumOfTwo(x, y) {
+//   // var a = 10 , b = 30;
+//   let total = y + x;
+//   console.log(total);
+// }
+// // console.log(sumOfTwo());  /// gives undefined
 
-/*
-Funciton Calling :-
-A javaScript function is executed when "something" invokes it (call it)
-*/
+// /*
 
-sumTwo();
-sumOfTwo(20, 12);
-sumOfTwo(3, 23);
-sumOfTwo(78, 435);
+// Funciton Calling :-
 
+// A javaScript function is executed when "something" invokes it (call it)
 
-
+// sumTwo();
+// sumOfTwo(20, 12);
+// sumOfTwo(3, 23);
+// sumOfTwo(78, 435);
 
 // Funtion Expressions
-// 
+// creat a function and put it into  the
+
+// function SUM(a, b) {
+//   var total = a + b;
+//   console.log(total);
+// }
+
+// var funExp = SUM(5, 2);
+
+// return keyword
+/*
+When js reaches a return statement , the function will stop executing.
+Function often compute a return value.
+The return value is "returned" back to the caller
+*/
+// var a, b;
+// function SUM(a, b) {
+//   return a + b;
+// }
+// var funExp = SUM(5, 2);
+// console.log("The sum of two number is " + funExp);
+
+// Anonymous function
+/*
+A function expression is similar to and has the same syntax as a function declaration
+one can define "name" function expressoion( where the name of the expression might be used in the call stack ) or "anonymous" function expressoion
+*/
+
+var funExp = function (a, b) {
+  return total = a + b;
+}
+// funExp(3, 23);  // not work
+var sum  = funExp(3,33);
+console.log(funExp(5,23));
+console.log(sum);
---
+++ JavaScripts/Modern_JavaScript/EcmaScript.js
@@ -0,0 +1,17 @@
+// History of JavaScript
+// JavaScript was created in 1996 
+// Ecma International :- 1997
+// ES6 (Ecma Script)6 :- 2015  , decided to switch to anual releases of EcmaScript
+
+
+// TimeLine
+// ES6(2015)  ES7(2016) , ES8(2017)...... nd more
+
+// Services
+// ECMASCRIPT 2015 / ES6 
+
+// let and const , destrubiting , template stings , object properties , 
+// default arguments , arrow funtion , rest operators , spread operators
+
+
+
