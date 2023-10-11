eb58978d2807c4e634110eb8f07168f8b0f49bfc
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-11 22:56:59 +0530

    EcmaScript Added

--- JavaScripts/Basics_of_JavaScript/Functions.js
+++ JavaScripts/Basics_of_JavaScript/Functions.js
@@ -16,42 +16,40 @@ SYNTAX :- function functionName(parameters){
 }
 */
 
-// // non-parameterised functions
-// function sumTwo() {
-//   var a = 10,
-//     b = 30;
-//   var total = a + b;
-//   console.log(total);
-// }
+// non-parameterised functions
+function sumTwo() {
+  var a = 10,
+    b = 30;
+  var total = a + b;
+  console.log(total);
+}
 
-// // Parameterised functions
-// function sumOfTwo(x, y) {
-//   // var a = 10 , b = 30;
-//   let total = y + x;
-//   console.log(total);
-// }
-// // console.log(sumOfTwo());  /// gives undefined
-
-// /*
+// Parameterised functions
+function sumOfTwo(x, y) {
+  // var a = 10 , b = 30;
+  let total = y + x;
+  console.log(total);
+}
+console.log(sumOfTwo()); /// gives undefined
 
 // Funciton Calling :-
 
 // A javaScript function is executed when "something" invokes it (call it)
 
-// sumTwo();
-// sumOfTwo(20, 12);
-// sumOfTwo(3, 23);
-// sumOfTwo(78, 435);
+sumTwo();
+sumOfTwo(20, 12);
+sumOfTwo(3, 23);
+sumOfTwo(78, 435);
 
 // Funtion Expressions
 // creat a function and put it into  the
 
-// function SUM(a, b) {
-//   var total = a + b;
-//   console.log(total);
-// }
+function SUM(a, b) {
+  var total = a + b;
+  console.log(total);
+}
 
-// var funExp = SUM(5, 2);
+var funExp = SUM(5, 2);
 
 // return keyword
 /*
@@ -59,12 +57,12 @@ When js reaches a return statement , the function will stop executing.
 Function often compute a return value.
 The return value is "returned" back to the caller
 */
-// var a, b;
-// function SUM(a, b) {
-//   return a + b;
-// }
-// var funExp = SUM(5, 2);
-// console.log("The sum of two number is " + funExp);
+var a, b;
+function SUM(a, b) {
+  return a + b;
+}
+var funExp = SUM(5, 2);
+console.log("The sum of two number is " + funExp);
 
 // Anonymous function
 /*
@@ -73,9 +71,9 @@ one can define "name" function expressoion( where the name of the expression mig
 */
 
 var funExp = function (a, b) {
-  return total = a + b;
-}
+  return (total = a + b);
+};
 // funExp(3, 23);  // not work
-var sum  = funExp(3,33);
-console.log(funExp(5,23));
+var sum = funExp(3, 33);
+console.log(funExp(5, 23));
 console.log(sum);
--- JavaScripts/Modern_JavaScript/EcmaScript.js
+++ JavaScripts/Modern_JavaScript/EcmaScript.js
@@ -1,17 +1,126 @@
 // History of JavaScript
-// JavaScript was created in 1996 
+// JavaScript was created in 1996
 // Ecma International :- 1997
 // ES6 (Ecma Script)6 :- 2015  , decided to switch to anual releases of EcmaScript
-
 
 // TimeLine
 // ES6(2015)  ES7(2016) , ES8(2017)...... nd more
 
 // Services
-// ECMASCRIPT 2015 / ES6 
+// ECMASCRIPT 2015 / ES6
 
-// let and const , destrubiting , template stings , object properties , 
-// default arguments , arrow funtion , rest operators , spread operators
+// 1:- let and const
+// 2:- template stings ,        destrubiting , object properties ,
+// 3:- default arguments ,        arrow funtion , rest operators , spread operators
 
+// *******************************************************************************
 
+// 1:-  Difference between var , let and const
 
+// var
+/*Function-scoped: Variables declared with var are function-scoped, which means they are only accessible within the function in which they are declared.
+
+Hoisting: Variables declared with var are hoisted to the top of their function or global scope. This means you can use a var variable before it's declared in the code, but it will be undefined until the declaration is reached.
+
+Reassignable: Variables declared with var can be reassigned.
+*/
+
+function Name() {
+  var firstame = "md";
+  console.log(firstame);
+
+  if (true) {
+    var lastName = "Akhlak";
+    console.log("First name: " + firstame);
+    console.log("last name: " + lastName);
+  }
+  console.log("first name +  lastName : = " + firstame + lastName);
+}
+Name();
+
+// *******************************************************************************
+
+// let
+/*
+Block-scoped: Variables declared with let are block-scoped, which means they are only accessible within the block (enclosed by curly braces) in which they are defined.
+
+Hoisting: Variables declared with let are hoisted, but they are not initialized, so you can't access them before the declaration.
+
+Reassignable: Variables declared with let can be reassigned.
+*/
+function Name() {
+  let firstame = "md";
+  console.log(firstame);
+
+  if (true) {
+    let lastName = "Akhlak";
+    console.log("First name: " + firstame);
+    console.log("last name: " + lastName);
+  }
+  console.log("first name +  lastName : = " + firstame + lastName); // can't access not in block
+}
+Name();
+
+// *******************************************************************************
+
+// const
+/*
+Block-scoped: Variables declared with const are also block-scoped, just like let.
+
+Hoisting: Like let, const variables are hoisted but not initialized, so you can't access them before the declaration.
+
+Immutable: Variables declared with const cannot be reassigned after they are initially assigned a value. However, the value itself may be mutable if it is an object or an array.
+*/
+function Name() {
+  const firstame = "md";
+  console.log(firstame);
+
+  if (true) {
+    const lastName = "Akhlak";
+    console.log("First name: " + firstame);
+    console.log("last name: " + lastName);
+  }
+  console.log("first name +  lastName : = " + firstame + lastName); // can't access not in block
+}
+Name();
+
+// *******************************************************************************
+
+// 3 :- Template Literals (Template string)
+
+for (let i = 1; i <= 10; i++) {
+  let tableOf = 10;
+  console.log(`${tableOf} * ${i} = ${i * tableOf}`);
+}
+
+const Add = () => `the sum of two number is : ${(a = 12) + (b = 43)}`;
+console.log(Add());
+
+// 4:- Default arguments / parameters
+
+function MULTI(a, b = 21) {
+  return a * b;
+}
+console.log(MULTI(3));
+
+//5:- Fat Arrow Functions
+
+// Normal function
+console.log("using traditional method of function");
+console.log(FUN());
+function FUN() {
+  let a = 12;
+  let b = 13;
+  let sum = a + b;
+  return `the sum of ${a} + ${b} = ${sum}`;
+}
+// convert normal function to fat arrow function
+console.log(`usng fat arrow method of funtion `);
+const sum = () => {
+  let x = 23;
+  let y = 21;
+  let s = y + x;
+  //   return `the sum is ${(x =121) + (y = 312)}`
+  return `the sum of ${x} + ${y} = ${s}`;
+};
+console.log(sum());
