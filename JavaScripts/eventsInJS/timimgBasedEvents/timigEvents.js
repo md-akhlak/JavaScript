dd4ceadc6b491769dcf7c6895ce31a468f4a5e68
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-30 01:21:01 +0530

    learned timing events in js

---
+++ JavaScripts/eventsInJS/timimgBasedEvents/timigEvents.js
@@ -0,0 +1,6 @@
+/*
+The window object allows execution of code at specified time intervals 
+-> these time intevals are called timing events
+-> the two key methods to use with javascript area 
+*/
+
